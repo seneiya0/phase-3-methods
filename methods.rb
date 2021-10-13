5c636469b1903772f055c5d95205a6a98b6edb25
seneiya <seneiyacooke@gmail.com>
2021-10-12 20:39:06 -0400

    commit tree bb45b7dfd5ccc73ee0291d904f013feacae9eba7

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name=nil)
+  name ||= "programmer"
+  puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+  num1 + num2
+end
+
+def halve(num)
+  if num.class == Integer
+    num / 2
+  else
+    nil
+  end
+end
